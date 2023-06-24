7da9289d78d0144a828e9dd1f782edfe3f9823a5
Jonas <jonas.abreuferreira@gmail.com>
2023-06-24 17:14:03 -0300

    feat: adding support for permissions control

--- iac/main.tf
+++ iac/main.tf
@@ -1,8 +1,14 @@
+locals {
+  iam_policy = file("iac/iam/policies/${var.environment}/policy.json")
+}
+
 module "glue_job" {
-  source = "https://github.com/JnsFerreira/terraform-module-aws-glue-job?ref=v1.0.0"
+  source = "git::https://github.com/JnsFerreira/terraform-module-aws-glue-job?ref=v1.0.0"
 
   name         = var.name
   description  = var.description
+  iam_policy   = local.iam_policy
   glue_version = var.glue_version
   command      = var.command
+  tags         = var.tags
 }
