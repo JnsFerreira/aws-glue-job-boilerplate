766175031cce43a6bb332e6943d85e9c443e1088
Jonas Ferreira <44999990+JnsFerreira@users.noreply.github.com>
2023-12-18 19:24:50 -0300

    chore: enabling job scheduling trough workflows

--- iac/main.tf
+++ iac/main.tf
@@ -12,3 +12,31 @@ module "glue_job" {
   command      = var.command
   tags         = var.tags
 }
+
+module "glue_workflow" {
+  source   = "cloudposse/glue/aws//modules/glue-workflow"
+  version  = "0.4.0"
+
+  workflow_name        = "my-workflow"
+  workflow_description = "Example of glue workflow"
+}
+
+module "glue_trigger" {
+  source   = "cloudposse/glue/aws//modules/glue-trigger"
+  version  = "0.4.0"
+
+  trigger_name        = "my-trigger"
+  workflow_name       = module.glue_workflow.name
+  trigger_description = "Triggers ${module.glue_job.name} Glue Job"
+  schedule            = var.schedule_expression
+  type                = "SCHEDULED"
+
+  actions = [
+    {
+      job_name = module.glue_job.name
+      timeout  = 300
+    }
+  ]
+
+  depends_on = [module.glue_job, module.glue_workflow]
+}
